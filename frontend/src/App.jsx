59d085bde1285d75bced79a742987bd019edd88c
niranjan7407 <palanisamyniranjan69@gmail.com>
2026-01-19 21:20:57 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="min-h-screen min-w-screen bg-gradient-to-tr from-purple-500 via-pink-500 to-red-500 flex  justify-center">
+        <h1 className="font-bold text-white text-4xl pt-10">OpenStream</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
